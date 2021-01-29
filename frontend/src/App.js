04a6bc83887647bb4743ebf5b7824fbc4d4a2c13
Ronald Esteban <ronaldbozesteban@gmail.com>
2021-01-28 22:08:34 -0800

    React setup

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="App">
+      <>
+      <h1>Welcome To ProShop</h1>
+      </>
+    </div>
+  );
+}
+
+export default App;
